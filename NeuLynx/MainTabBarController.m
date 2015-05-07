27807e8e305dc7f3aeb79a0b0651b6f4255a32e6
Ronald Hernandez <2ronald.hernandez@gmail.com>
2015-05-07 03:33:55 -0400

    added sign in vc

--- NeuLynx/MainTabBarController.m
+++ NeuLynx/MainTabBarController.m
@@ -30,13 +30,24 @@
     UITabBarController *mapNavVC = [mapStoryboard instantiateViewControllerWithIdentifier:@"MapNavVC"];
     [self addChildViewController:mapNavVC];
     mapNavVC.tabBarItem.image = [self imageWithImage:[UIImage imageNamed:@"mapIcon"] scaledToSize:CGSizeMake(35, 35)];
+    mapNavVC.tabBarItem.title = @"Map";
 
-    //create the first tab - get the storybody, then use the storyboard to present the first view controller
+    //create the second tab - get the storybody, then use the storyboard to present the first view controller
     //also set an image for the first tab.
     UIStoryboard *profileStoryboard = [UIStoryboard storyboardWithName:@"Profile" bundle:nil];
     UITabBarController *profileNavVC = [profileStoryboard instantiateViewControllerWithIdentifier:@"profileNavVC"];
     [self addChildViewController:profileNavVC];
     profileNavVC.tabBarItem.image = [self imageWithImage:[UIImage imageNamed:@"profileIcon"] scaledToSize:CGSizeMake(35, 35)];
+    profileNavVC.tabBarItem.title = @"Profile";
+
+    //create the third tab - get the storybody, then use the storyboard to present the first view controller
+    //also set an image for the first tab.
+    UIStoryboard *mailStoryboard = [UIStoryboard storyboardWithName:@"Mail" bundle:nil];
+    UITabBarController *mailNavVC = [mailStoryboard instantiateViewControllerWithIdentifier:@"mailNavVC"];
+    [self addChildViewController:mailNavVC];
+    mailNavVC.tabBarItem.image = [self imageWithImage:[UIImage imageNamed:@"mailIcon"] scaledToSize:CGSizeMake(35, 35)];
+    mailNavVC.tabBarItem.title = @"Messages";
+
 }
 - (UIImage *)imageWithImage:(UIImage *)image scaledToSize:(CGSize)newSize {
     UIGraphicsBeginImageContextWithOptions(newSize, NO, 0.0);
---
+++ NeuLynx/SignInVC.h
@@ -0,0 +1,13 @@
+//
+//  SignInVC.h
+//  NeuLynx
+//
+//  Created by Ronald Hernandez on 5/7/15.
+//  Copyright (c) 2015 NeuLynx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SignInVC : UIViewController
+
+@end
