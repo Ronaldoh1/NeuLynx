dc040d23e5294a9d7953145f171d6b4b36b32545
Ronald Hernandez <2ronald.hernandez@gmail.com>
2015-06-13 20:15:20 -0400

    added ui for detail vc

--- NeuLynx/DetailVC.m
+++ NeuLynx/DetailVC.m
@@ -7,9 +7,11 @@
 //
 
 #import "DetailVC.h"
+#import <RateView.h>
 
 
 @interface DetailVC ()
+@property (weak, nonatomic) IBOutlet UIView *uiview;
 
 @end
 
@@ -28,6 +30,17 @@
     titleView.text = @"Activity Detail";
     titleView.textColor = [UIColor colorWithRed:34/255.0 green:152/255.0 blue:212/255.0 alpha:1];
     [self.navigationItem setTitleView:titleView];
+
+
+    RateView* rv = [RateView rateViewWithRating:3.7f];
+    rv.starFillColor = [UIColor colorWithRed:34/255.0 green:152/255.0 blue:212/255.0 alpha:1];
+    rv.frame = CGRectMake(115, 291, 150, 30);
+
+    [self.view addSubview:rv];
+
+
+
+
 }
 
 ///*************ACTION BUTTONS***************************//
