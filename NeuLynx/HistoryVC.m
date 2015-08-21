d0f9eeb35e49690479bcac1ce2f9f7fb8c1b6722
Ronald Hernandez <2ronald.hernandez@gmail.com>
2015-08-21 04:26:24 -0700

    able to display history

--- NeuLynx/HistoryVC.m
+++ NeuLynx/HistoryVC.m
@@ -35,6 +35,8 @@
     [self.navigationItem setTitleView:titleView];
 
     [self downloadActivitiesJoinedForCurrentUser];
+
+    [self downloadActivitiesPostedForCurrentUser];
 }
 - (IBAction)onDoneButtonTapped:(UIBarButtonItem *)sender {
     [self dismissViewControllerAnimated:YES completion:nil];
@@ -66,13 +68,13 @@
 
 }
 
--(void)downloadActivitiesJoinedForCurrentUser{
+-(void)downloadActivitiesPostedForCurrentUser{
 
 
-    PFQuery *historyQuery = [PFQuery queryWithClassName:@"History"];
-    [historyQuery whereKey:@"user" equalTo:[User currentUser]];
-    [historyQuery includeKey:@"activityJoined"];
-    [historyQuery  includeKey:@"activityJoined.host"];
+    PFQuery *historyQuery = [PFQuery queryWithClassName:@"Activity"];
+    [historyQuery whereKey:@"host" equalTo:[User currentUser]];
+    [historyQuery includeKey:@"host"];
+
 
 
 //    PFQuery *query = [History query];
@@ -89,14 +91,14 @@
 
             NSLog(@"%@", historyArray);
             //get a copy of all activities
-            self.acceptedHistoryArray = [NSMutableArray arrayWithArray:historyArray];
+            self.postedHistoryArray = [NSMutableArray arrayWithArray:historyArray];
             // Add activities to the map.
             if (self.acceptedHistoryArray.count == 0) {
 
                 UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"No Activities Yet 😕" message:@"You haven't joined any activities. Be adventurous and find activities happening near you. Simply head over to the map and check them out!" delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
 
 
-                [alertView show];
+                //[alertView show];
             }
 
             
@@ -109,6 +111,47 @@
 }
 
 
+-(void)downloadActivitiesJoinedForCurrentUser{
+
+
+    PFQuery *historyQuery = [PFQuery queryWithClassName:@"History"];
+    [historyQuery whereKey:@"user" equalTo:[User currentUser]];
+    [historyQuery includeKey:@"activityJoined"];
+    [historyQuery  includeKey:@"activityJoined.host"];
+
+
+    //    PFQuery *query = [History query];
+    //
+    //    [query whereKey:@"user" equalTo:[User currentUser]];
+    //    [query includeKey:@"activity"];
+    //    [query includeKey:@"exclusiveInvitee"];
+
+
+    [historyQuery findObjectsInBackgroundWithBlock:^(NSArray *historyArray, NSError *error){
+
+
+        if (!error) {
+
+            //NSLog(@"%@", historyArray);
+            //get a copy of all activities
+            self.acceptedHistoryArray = [NSMutableArray arrayWithArray:historyArray];
+            // Add activities to the map.
+            if (self.acceptedHistoryArray.count == 0) {
+
+                UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"No Activities Yet 😕" message:@"You haven't joined any activities. Be adventurous and find activities happening near you. Simply head over to the map and check them out!" delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+
+
+                [alertView show];
+            }
+            
+            
+        }
+        [self.tableView reloadData];
+        
+    }];
+    
+    
+}
 
 #pragma marks - TableView Delegate Methods
 
@@ -133,7 +176,13 @@
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
 
-    return [self.acceptedHistoryArray count];
+    if (section == 0) {
+        return [self.acceptedHistoryArray count];
+    }else{
+        return [self.postedHistoryArray count];
+    }
+
+
 }
 
 -(HistoryCustomCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
@@ -145,6 +194,9 @@
 
     //change the color of scrollbar
     tableView.indicatorStyle = UIScrollViewIndicatorStyleWhite;
+
+
+    if (indexPath.section == 0) {
 
 
 
@@ -188,10 +240,109 @@
 
 
     return cell;
+    }else{
+
+
+        //Display activities that were posted by current user.
+
+        //History *historyRecord  = (History *)self.acceptedHistoryArray[indexPath.row];
+
+        Activity *activity =  [Activity new];
+
+        activity =  (Activity *)self.postedHistoryArray[indexPath.row];
+
+        [activity fetchIfNeededInBackground];
+
+
+        User *user = [User new];
+
+        user = (User *)activity.host;
+
+
+        [user.profileImage getDataInBackgroundWithBlock:^(NSData *data, NSError *error) {
+
+            if (!error) {
+
+                dispatch_async(dispatch_get_main_queue(), ^{
+
+                    UIImage *image = [UIImage imageWithData:data];
+                    cell.userProfileImage.image = image;
+
+                });
+            }else{
+                NSLog(@" %@       errorrrrrr %@", error.localizedDescription, error.description);
+            }
+            
+            
+        }];
+        
+        cell.activityTitleLabel.text = activity.activityTitle;
+        cell.activityDescriptionText.text = activity.activityDescription;
+
+
+
+
+        return cell;
+    }
     
 }
 
+- (void)tableView:(UITableView *)tableView accessoryButtonTappedForRowWithIndexPath:(NSIndexPath *)indexPath{
+    //[self performSegueWithIdentifier:@"toDetail" sender:indexPath];//IndexPath as senderi
+    if (indexPath.section == 0) {
 
 
+
+        UIStoryboard *detailStoryboard = [UIStoryboard storyboardWithName:@"Detail" bundle:nil];
+        UIViewController *detailVC = [detailStoryboard instantiateViewControllerWithIdentifier:@"detailNavVc"];
+
+        //set the activity that is goign to be shared through out the app - to dispay to the user when the user clicks on detail.
+        AppDelegate *appDelegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
+
+
+        appDelegate.sharedActivity = (Activity *)(((History *)self.acceptedHistoryArray[indexPath.row]).activityJoined);
+        //appDelegate.exclusiveInvite = ((ExclusiveInvite *)self.exclusiveInvitesArray[indexPath.row]);
+
+        [self presentViewController:detailVC animated:YES completion:nil];
+
+
+
+    }else if(indexPath.section == 1){
+
+
+        UIStoryboard *detailStoryboard = [UIStoryboard storyboardWithName:@"Detail" bundle:nil];
+        UIViewController *detailVC = [detailStoryboard instantiateViewControllerWithIdentifier:@"detailNavVc"];
+        detailVC.navigationItem.rightBarButtonItem = nil;
+
+        //set the activity that is goign to be shared through out the app - to dispay to the user when the user clicks on detail.
+        AppDelegate *appDelegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
+
+
+        appDelegate.sharedActivity = (Activity *)(self.postedHistoryArray[indexPath.row]);
+        //appDelegate.exclusiveInvite = ((ExclusiveInvite *)self.exclusiveInvitesArray[indexPath.row]);
+
+        [self presentViewController:detailVC animated:YES completion:nil];
+
+    }
+}
+
+
+-(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
+
+//    if(){
+//
+////        DetailViewController *destinationVC = (DetailViewController *)segue.destinationViewController;
+////        destinationVC.icon = self.icon;
+//        NSLog(@"hiiiiiiii");
+//
+//    }else if([segue.identifier isEqual:@"goToRating"]){
+////        RatingTableViewController *destinationVC = (RatingTableViewController *)segue.destinationViewController;
+////        destinationVC.icon = self.icon;
+//
+//        NSLog(@"Byeee");
+//    }
+//    
+
+}
 
 @end
